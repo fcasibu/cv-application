01681d04727ee091ebe61bbbfe33010215c138f8
nevz <75290989+nevz9@users.noreply.github.com>
2022-04-09 23:44:58 +0800

    Change title and added preview/edit buttons

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,31 @@
+import { useState } from "react";
+
 function Header(props) {
+  const editorClickHandler = () => {
+    props.onChangeMode("editor");
+  };
+
+  const previewClickHandler = () => {
+    props.onChangeMode("preview");
+  };
+
   return (
     <div className="self-center bg-white p-5">
-      <div>Hello World</div>
+      <h1 className="font-bold text-3xl">Create your CV!</h1>
+      <div className="flex gap-2 justify-center">
+        <button
+          onClick={editorClickHandler}
+          className="bg-slate-500 text-white font-bold p-2 rounded mt-3"
+        >
+          Editor Mode
+        </button>
+        <button
+          onClick={previewClickHandler}
+          className="bg-slate-500 text-white font-bold p-2 rounded mt-3"
+        >
+          Preview Mode
+        </button>
+      </div>
     </div>
   );
 }
