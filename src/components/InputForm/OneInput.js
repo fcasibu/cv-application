253e713c31e18a0dbc05c69a6a597692ccb8e6fd
nevz <75290989+nevz9@users.noreply.github.com>
2022-04-09 14:41:02 +0800

    Small changes
    Transferred most functions from Skills/Interests in here

--- src/components/InputForm/OneInput.js
+++ src/components/InputForm/OneInput.js
@@ -8,15 +8,33 @@ const OneInput = (props) => {
     props.onGetValue(event.target.value);
   };
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const obj = {
+      id: props.id,
+      name: input,
+    };
+    props.onAdd(obj);
+    setInput("");
+  };
+
   return (
     <div>
-      <input
-        type="text"
-        value={props.isDone ? "" : input}
-        onChange={changeInputHandler}
-        className="w-full p-2"
-        placeholder={props.inputName}
-      />
+      <form onSubmit={submitHandler}>
+        <input
+          type="text"
+          value={input}
+          onChange={changeInputHandler}
+          className="w-full p-2 border-2 border-slate-500"
+          placeholder={props.inputName}
+        />
+        <button
+          type="submit"
+          className="bg-slate-500 text-white font-bold p-2 rounded mt-3"
+        >
+          + Add
+        </button>
+      </form>
     </div>
   );
 };
