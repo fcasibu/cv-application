b3e3c1b36c518a84c315f025dffd23781d20a958
nevz <75290989+nevz9@users.noreply.github.com>
2022-04-09 14:41:44 +0800

    Add Interests Component

--- src/components/Main/EditorMode/LeftMain/Interests/Interests.js
+++ src/components/Main/EditorMode/LeftMain/Interests/Interests.js
@@ -1,13 +1,41 @@
-import InputComponent from "../../../../InputForm/InputForm";
+import { useState } from "react";
+import OneInput from "../../../../Forms/OneInput";
 
 const Interests = (props) => {
+  const getValue = (value) => {
+    props.onChangeInterest(value);
+  };
+
+  const deleteClickHandler = (event) => {
+    const filteredList = props.interests.filter(
+      (el) => el.id !== event.target.id
+    );
+    props.onDeleteInterest(filteredList);
+  };
+
   return (
     <div>
-      <h2 className="text-lg">Interests</h2>
-      <form>
-        <InputComponent inputName="Machine Learning" />
-        <button className="bg-white p-2 rounded mt-3">+ Add</button>
-      </form>
+      <h2 className="text-lg font-bold">Interests</h2>
+      {props.interests.map((el) => {
+        return (
+          <div key={el.id} className="flex justify-between items-center mb-2">
+            <div className="text-slate-700 font-bold">{el.name}</div>
+            <button
+              className="text-slate-700 cursor-pointer font-bold"
+              id={el.id}
+              onClick={deleteClickHandler}
+            >
+              X
+            </button>
+          </div>
+        );
+      })}
+      <OneInput
+        inputName="Machine Learning"
+        onGetValue={getValue}
+        onAdd={props.onAddInterest}
+        id={props.interestsId}
+      />
     </div>
   );
 };
