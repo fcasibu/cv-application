8c2b8e420c731a104697dd470cfb683ef2185e14
nevz <75290989+nevz9@users.noreply.github.com>
2022-04-09 14:39:18 +0800

    Add interest/skills components

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      id: uniqid(),
+      skillsId: uniqid(),
+      interestsId: uniqid(),
       formDetails: {
         aboutMe: "",
         skill: "",
@@ -45,7 +46,11 @@ class App extends Component {
 
     this.changePersonalInfo = this.changePersonalInfo.bind(this);
     this.changeSkill = this.changeSkill.bind(this);
+    this.changeInterest = this.changeInterest.bind(this);
     this.addSkill = this.addSkill.bind(this);
+    this.addInterest = this.addInterest.bind(this);
+    this.deleteSkill = this.deleteSkill.bind(this);
+    this.deleteInterest = this.deleteInterest.bind(this);
   }
 
   changePersonalInfo(personalInfoObj) {
@@ -72,32 +77,93 @@ class App extends Component {
     });
   }
 
+  changeInterest(interestDetail) {
+    this.setState({
+      formDetails: {
+        interest: interestDetail,
+      },
+    });
+  }
+
   addSkill(skillDetail) {
     this.setState((state) => {
       return {
-        id: uniqid(),
+        skillsId: uniqid(),
         multipleDetails: {
+          workExperience: [...state.multipleDetails.workExperience],
           skills: [...state.multipleDetails.skills, skillDetail],
+          interests: [...state.multipleDetails.interests],
+        },
+      };
+    });
+  }
+
+  addInterest(interestDetail) {
+    this.setState((state) => {
+      return {
+        interestsId: uniqid(),
+        multipleDetails: {
+          workExperience: [...state.multipleDetails.workExperience],
+          skills: [...state.multipleDetails.skills],
+          interests: [...state.multipleDetails.interests, interestDetail],
+        },
+      };
+    });
+  }
+
+  deleteSkill(newSkills) {
+    this.setState((state) => {
+      return {
+        multipleDetails: {
+          workExperience: state.multipleDetails.workExperience,
+          skills: newSkills,
+          interests: state.multipleDetails.interests,
+        },
+      };
+    });
+  }
+
+  deleteInterest(newInterests) {
+    this.setState((state) => {
+      return {
+        multipleDetails: {
+          workExperience: state.multipleDetails.workExperience,
+          skills: state.multipleDetails.skills,
+          interests: newInterests,
         },
       };
     });
   }
 
   render() {
-    const { changePersonalInfo, changeSkill, addSkill } = this;
-    const { personalInfo, skill } = this.state.formDetails;
-    const { skills } = this.state.multipleDetails;
+    const {
+      changePersonalInfo,
+      changeSkill,
+      changeInterest,
+      addSkill,
+      deleteSkill,
+      addInterest,
+      deleteInterest,
+    } = this;
+    const { personalInfo, skill, interest } = this.state.formDetails;
+    const { skills, interests } = this.state.multipleDetails;
     return (
-      <div className="App">
-        <div className="min-h-screen container max-w-3xl mx-auto flex flex-col gap-10 p-10 bg-slate-500">
+      <div className="App bg-slate-800">
+        <div className="min-h-screen container max-w-3xl mx-auto flex flex-col gap-10 p-10 bg-white">
           <Header />
           <Main
+            interestsId={this.state.interestsId}
+            skillsId={this.state.skillsId}
             onChangePersonalInfo={changePersonalInfo}
             personalInfo={personalInfo}
             onChangeSkill={changeSkill}
+            onChangeInterest={changeInterest}
             onAddSkill={addSkill}
+            onAddInterest={addInterest}
             skills={skills}
-            uniqueId={this.state.id}
+            interests={interests}
+            onDeleteSkill={deleteSkill}
+            onDeleteInterest={deleteInterest}
           />
         </div>
       </div>
